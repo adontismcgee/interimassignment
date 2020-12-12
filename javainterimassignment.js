390bb086cd48f33289901752cbc60f4e8dcf0a06
adontismcgee <69471413+adontismcgee@users.noreply.github.com>
2020-12-11 22:11:47 -0600

    Add files via upload

---
+++ javainterimassignment.js
@@ -0,0 +1,21 @@
+let score="60"
+let letter=""
+if(score>=85){
+  letter="A";
+}
+else if(score>=85){
+  letter="A-";
+}
+else if(score>=80){
+  letter="B";
+}
+else if(score>=70){
+  letter="C";
+}
+else if(score>=60){
+  letter="D";
+}
+else if(score>=40){
+  letter="E";
+}
+console.log(letter);
